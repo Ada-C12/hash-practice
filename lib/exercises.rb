3613531861b91a7ad8592bdf09e2f3f60a6c0850
stupendousC <CarolineWuKaplan@gmail.com>
2020-03-05 16:48:52 -0800

    test passed for grouped_anagrams() and top_k_frequent_elements()

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -2,19 +2,67 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) + O(m log m)  where n = strings[] size and m = length of each word therein.
+# Space Complexity: O(n) 
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  
+  inventory = {}
+  # key = alphabetically sorted word
+  # value = array of original word(s), where each can be sorted into the key.
+  # example: "bat" from strings[] would show up in inventory as "abt" => ["bat"]
+  # and later if "tab" is found in strings[], inventory would now include "abt" => ["bat", "tab"]
+  
+  strings.each do |word|
+    # source for line below: https://solidfoundationwebdev.com/blog/posts/how-to-sort-a-string-alphabetically-in-ruby
+    sortedWord = word.chars.sort_by(&:downcase).join
+    
+    if inventory[sortedWord]
+      inventory[sortedWord] << word 
+    else
+      inventory[sortedWord] = [word]
+    end
+  end
+  
+  output = []
+  inventory.each do |key, value|
+    output << value
+  end
+  
+  return output
 end
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) + O(m log m) where n = size of list and m = number of keys in inventory{}
+# Space Complexity: O(n)
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return [] if list == []
+  
+  inventory = {}
+  # take inventory of list, where key = element and value = #_of_occurrences
+  list.each do |element|
+    if inventory[element]
+      inventory[element] += 1
+    else
+      inventory[element] = 1
+    end
+  end
+  
+  # sort the hash values and return as a 2D array
+    # weirdness, if I sorted by value to get the asc_order, then the order of appearance in orig list will be backwards :=(
+    # but if I sort by -value, then order of appearance is preserved and tests will pass
+  key_value_arrays_in_desc_order = inventory.sort_by { |key, value| -value }
+  p key_value_arrays_in_desc_order
+  
+  answer = []
+  index = 0
+  until index == k
+    answer << key_value_arrays_in_desc_order[index][0]
+    index += 1
+  end
+  
+  return answer
 end
 
 
--- test/exercises_test.rb
+++ test/exercises_test.rb
@@ -9,33 +9,33 @@ describe "exercises" do
       # Act-Assert
       expect(grouped_anagrams(list)).must_equal []
     end
-
+    
     it "will work for the README example" do
       # Arrange
       list = ["eat", "tea", "tan", "ate", "nat", "bat"]
-
+      
       # Act
       answer = grouped_anagrams(list)
-
+      
       expected_answer = [
         ["ate","eat","tea"],
         ["nat","tan"],
         ["bat"]
       ]
-
+      
       # Assert
       answer.each_with_index do |array, index|
         expect(expected_answer).must_include array.sort
       end
     end
-
+    
     it "will work for strings with no anagrams" do
       # Arrange
       list = ["eat", "ear", "tar", "pop", "pan", "pap"]
-
+      
       # Act
       answer = grouped_anagrams(list)
-
+      
       expected_answer = [
         ["eat"],
         ["ear"],
@@ -44,20 +44,20 @@ describe "exercises" do
         ["pan"],
         ["pap"]
       ]
-
+      
       # Assert
       answer.each_with_index do |array, index|
         expect(expected_answer).must_include array.sort
       end
     end
-
+    
     it "will work for strings that are all anagrams" do
       # Arrange
       list = ["eat", "tae", "tea", "eta", "aet", "ate"]
-
+      
       # Act
       answer = grouped_anagrams(list)
-
+      
       expected_answer = [
         [ "aet", "ate", "eat", "eta", "tae", "tea"]
       ]
@@ -67,70 +67,70 @@ describe "exercises" do
       end
     end
   end
-
-  xdescribe "top_k_frequent_elements" do
+  
+  describe "top_k_frequent_elements" do
     it "works with example 1" do
       # Arrange
       list = [1,1,1,2,2,3]
       k = 2
-
+      
       # Act
       answer = top_k_frequent_elements(list, k)
-
+      
       # Assert
       expect(answer.sort).must_equal [1,2]
     end
-
+    
     it "works with example 2" do
       # Arrange
       list = [1]
       k = 1
-
+      
       # Act
       answer = top_k_frequent_elements(list, k)
-
+      
       # Assert
       expect(answer.sort).must_equal [1]
     end
-
+    
     it "will return [] for an empty array" do
       # Arrange
       list = []
       k = 1
-
+      
       # Act
       answer = top_k_frequent_elements(list, k)
-
+      
       # Assert
       expect(answer.sort).must_equal []
     end
-
+    
     it "will work for an array with k elements all unique" do
       # Arrange
       list = [1, 2, 3]
       k = 3
-
+      
       # Act
       answer = top_k_frequent_elements(list, k)
-
+      
       # Assert
       expect(answer.sort).must_equal [1, 2, 3]
     end
-
+    
     it "will work for an array when k is 1 and several elements appear 1 time (HINT Pick the 1st one)" do
       # Arrange
       list = [1, 2, 3]
       k = 1
-
+      
       # Act
       answer = top_k_frequent_elements(list, k)
-
+      
       # Assert
       expect(answer.sort).must_equal [1]
     end
-
+    
   end
-
+  
   xdescribe "valid sudoku" do
     it "works for the table given in the README" do
       # Arrange
@@ -145,14 +145,14 @@ describe "exercises" do
         [".",".",".","4","1","9",".",".","5"],
         [".",".",".",".","8",".",".","7","9"]
       ]
-
+      
       # Act
       valid = valid_sudoku(table)
-
+      
       # Assert
       expect(valid).must_equal true
     end
-
+    
     it "fails for the table given in the README" do
       # Arrange
       table = [
@@ -166,14 +166,14 @@ describe "exercises" do
         [".",".",".","4","1","9",".",".","5"],
         [".",".",".",".","8",".",".","7","9"]
       ]
-
+      
       # Act
       valid = valid_sudoku(table)
-
+      
       # Assert
       expect(valid).must_equal false
     end
-
+    
     it "fails for a duplicate number in a sub-box" do
       # Arrange
       table = [
@@ -187,14 +187,14 @@ describe "exercises" do
         [".",".",".","4","1","9",".",".","5"],
         [".",".",".",".","8",".",".","7","9"]
       ]
-
+      
       # Act
       valid = valid_sudoku(table)
-
+      
       # Assert
       expect(valid).must_equal false
     end
-
+    
     it "fails for a duplicate number in a bottom right sub-box" do
       # Arrange
       table = [
@@ -208,10 +208,10 @@ describe "exercises" do
         [".",".",".","4","1","9","8",".","5"],
         [".",".",".",".","8",".",".","7","9"]
       ]
-
+      
       # Act
       valid = valid_sudoku(table)
-
+      
       # Assert
       expect(valid).must_equal false
     end
