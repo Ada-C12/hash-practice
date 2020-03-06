dc334fcbcd7e92d0483bb19602e024ac5e300d20
Sabrina Lowney <slowne@ext.uber.com>
2020-03-05 16:33:09 -0800

    grouped-anagrams

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -2,11 +2,38 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
-
+# Time Complexity: O(n) where n is the number of strings
+# Space Complexity: O(n) where n is the number of strings
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  length = strings.length
+  # kind of arbitrary but it seems big enough to get a good spread?
+  upper_bound = length * 3
+  hash = Array.new(upper_bound)
+
+  strings.each do |string|
+    hashed_index = get_hashed_index(string, upper_bound)
+    if hash[hashed_index].nil?
+      hash[hashed_index] = [string]
+    else
+      hash[hashed_index].push(string)
+    end
+  end
+
+  return get_clean_hash(hash)
+end
+
+def get_hashed_index(string, upper_bound)
+  return string.bytes.sum % upper_bound
+end
+
+def get_clean_hash(hash)
+  clean = []
+  hash.each do |item|
+    if !item.nil?
+      clean << item
+    end
+  end
+  return clean
 end
 
 # This method will return the k most common elements
@@ -21,7 +48,7 @@ end
 # This method will return the true if the table is still
 #   a valid sudoku table.
 #   Each element can either be a ".", or a digit 1-9
-#   The same digit cannot appear twice or more in the same 
+#   The same digit cannot appear twice or more in the same
 #   row, column or 3x3 subgrid
 # Time Complexity: ?
 # Space Complexity: ?
