d273bb3394ec27a1cd3f7189c0f8f2a7b570868e
Farah Davoodi <farahdavoodi@gmail.com>
2020-03-05 16:33:08 -0800

    passed anagrams tests

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -2,11 +2,26 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n*m) where n is the length of the strings area and m is the length of the string/num of characters
+# Space Complexity: O(n) where n is the size of the strings array
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return [] if strings.empty?
+
+  #create empty hash
+  hash = {}
+
+  #iterate and alphabetize
+  strings.each do |word|
+    alpha = word.chars.sort.join
+    if hash[alpha]
+      hash[alpha] << word
+    else
+      hash[alpha] = [word]
+    end
+  end
+
+  return hash.values
 end
 
 # This method will return the k most common elements
