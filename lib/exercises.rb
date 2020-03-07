448fd46c6c5020c91aafff5c845259db7fe29a83
Ball <fasv@M-C02YN1MCJG5J.nordstrom.net>
2020-03-06 18:55:59 -0800

    refactor/passing tests

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -4,6 +4,7 @@
 # Space Complexity: O(n * m) where n is number of strings in array and m is number of charcters in longest string
 def create_char_hash(string)
   output_hash = {}
+  
   string.each_char do |char|
     if output_hash[char]
       output_hash[char] += 1
@@ -14,7 +15,6 @@ def create_char_hash(string)
 
   return output_hash
 end
-
 
 def grouped_anagrams(strings)
   anagrams = {}
@@ -43,8 +43,8 @@ end
 def top_k_frequent_elements(list, k)
   return [] if list.empty?
   raise ArgumentError.new("K can't be larger then length of list") if k > list.length
+  
   nums = {}
-
   list.each do |num|
     if nums[num]
       nums[num] += 1
@@ -63,31 +63,34 @@ def top_k_frequent_elements(list, k)
   return output
 end
 
-
 # This method will return the true if the table is still
 #   a valid sudoku table.
 #   Each element can either be a ".", or a digit 1-9
 #   The same digit cannot appear twice or more in the same 
 #   row, column or 3x3 subgrid
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is length of table input (size sudoku)
+# Space Complexity: O(n) where n is length of table input (size sudoku)
 def generate_values_checker
   values = {}
+  
   value = 1
   9.times do
     values[value] = false
     value += 1
   end
+  
   return values
 end
 
 def generate_rows_cols_checker
   rows_cols = {}
+  
   value = 0
   9.times do
     rows_cols[value] = generate_values_checker
     value += 1
   end
+  
   return rows_cols
 end
 
@@ -98,18 +101,21 @@ def generate_subgrids_checker
   
   3.times do
     row += 1
+    
     3.times do
       col += 1
       grid = [row, col]
       subgrids[grid] = generate_values_checker
     end
+    
     col = 0
   end
+  
   return subgrids
 end
 
-
 def valid_sudoku(table)
+  # valid for 9x9 soduku board
   return false if table.length > 9
   
   subgrids = generate_subgrids_checker
@@ -118,6 +124,7 @@ def valid_sudoku(table)
 
   table.each_with_index do |row, row_index|
     row.each_with_index do |val, col_index|
+      
       if val != "."
         grid = [(col_index/3 + 1), (row_index/3 + 1) ]
         return false if subgrids[grid][val]
@@ -129,7 +136,11 @@ def valid_sudoku(table)
         return false if cols[col_index][val]
         cols[col_index][val] = true;
       end
+    
+      # checks if table is not a square
+      return false if col_index > table.length - 1
     end
   end
+  
   return true
 end
