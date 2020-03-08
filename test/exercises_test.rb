1d0359039f3f32462758374e0d25983c25c88558
Emily Vomacka <emilyvomacka@gmail.com>
2020-03-07 22:13:37 -0800

    passing

--- test/exercises_test.rb
+++ test/exercises_test.rb
@@ -68,7 +68,7 @@ describe "exercises" do
     end
   end
 
-  xdescribe "top_k_frequent_elements" do
+  describe "top_k_frequent_elements" do
     it "works with example 1" do
       # Arrange
       list = [1,1,1,2,2,3]
@@ -131,7 +131,7 @@ describe "exercises" do
 
   end
 
-  xdescribe "valid sudoku" do
+  describe "valid sudoku" do
     it "works for the table given in the README" do
       # Arrange
       table = [
