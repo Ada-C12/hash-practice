5adaab95b6c2fffb9107ae2e7400f9109bd42f7d
Sara Shah Baig <sarashahbaig@gmail.com>
2020-03-08 22:38:13 -0700

    completed and tests passing

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -2,19 +2,53 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: o(n)
+# Space Complexity: o(n)
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  # raise NotImplementedError, "Method hasn't been implemented yet!"
+  hash = {}
+
+  strings.each do |word|
+    key = word.chars.sort.join
+
+    if hash[key]
+      hash[key] << word
+    else
+      hash[key] = [word]
+    end
+  end
+
+  return hash.values
 end
+
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: o(n)
+# Space Complexity: o(n)
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  # raise NotImplementedError, "Method hasn't been implemented yet!"
+  hash = {}
+  result = []
+
+  return result if list.empty?
+
+  list.each do |num|
+    if hash[num]
+      hash[num] += 1
+    else
+      hash[num] = 1
+    end
+  end
+
+  k.times do
+    max = hash.key(hash.values.max)
+    result << max
+    hash.delete(max)
+  end
+
+  return result
 end
 
 
