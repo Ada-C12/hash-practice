9a4c667b9a61e11a10c107ac71c88ecccfa3fb4c
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2020-03-08 19:18:57 -0700

    Solved top k and grouped anagrams

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,4 +1,4 @@
-
+require 'awesome_print'
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
@@ -6,7 +6,46 @@
 # Space Complexity: ?
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return [] if strings.empty?
+
+  hash_groups = []
+  anagrams_grouped = []
+
+  strings.each do |string|
+    string_hash = {}
+
+    # count freq of characters in string through hash
+    string.each_char do |char|
+      if string_hash[char]
+        string_hash[char] += 1
+      else
+        string_hash[char] = 1
+      end
+    end
+
+    # if no hash_groups exist, add new string hash to hash group and add string to anagrams list
+    if hash_groups.empty?
+      hash_groups << string_hash
+      anagrams_grouped << [string]
+    
+    # otherwise, test string hash against all previous hashes, if match, add to existing anagram group, if unique, add hash group, create new anagram group
+    else
+      unique = true
+      hash_groups.each_with_index do |hash, index|
+        if hash == string_hash
+          unique = false
+          anagrams_grouped[index] << string
+        end
+      end
+
+      if unique
+        hash_groups << string_hash
+        anagrams_grouped << [string]
+      end
+    end
+  end
+
+  return anagrams_grouped
 end
 
 # This method will return the k most common elements
@@ -14,7 +53,31 @@ end
 # Time Complexity: ?
 # Space Complexity: ?
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return [] if list.empty? || k == 0
+
+  item_with_freq = []
+  item_freq = {}
+
+  # make hash with freq of each element
+  list.each do |item|
+    if item_freq[item]
+      item_freq[item] += 1
+    else
+      item_freq[item] = 1
+    end
+  end
+
+  # turn hash into array and sort by freq
+  item_with_freq = item_freq.to_a
+  item_with_freq.sort_by { |item| item[1] }
+
+  # iterate through sorted array k times and pull out top items
+  top_items = []
+  k.times do |i|
+    top_items << item_with_freq[i][0]
+  end
+
+  return top_items
 end
 
 
