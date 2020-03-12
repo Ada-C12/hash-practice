aaf55a9eb169570782ededcc2c1191325d5c4f44
daniela sanchez <dnsanche@gmail.com>
2020-03-11 21:07:21 -0700

    anagrams

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -2,11 +2,45 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: n^2 as for each word in the list I'm iterating over the letters.
+
+# # Space Complexity: O(n) because if none of the words are anagrams, 
+# I will duplicate the array. Also, I will create a hash were each key is each word.
+
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  alphabet = {}
+  words_same_value = {}
+  
+  index = 0
+  anagram_array = []
+  value = 0
+  
+    strings.each do |word|
+      word_value = 0
+
+      word.each_char do |letter|  
+        if alphabet[letter] == nil
+          alphabet[letter] = value + 1
+          value += 1
+          word_value += alphabet[letter]
+        else
+          word_value += alphabet[letter]
+        end
+      end
+
+      if words_same_value[word_value] == nil
+        words_same_value[word_value] = [word] 
+      else 
+        words_same_value[word_value] << word
+      end
+    end
+
+    words_same_value.keys do |sum|
+      anagram_array << sum
+    end  
+
+    return anagram_array
 end
 
 # This method will return the k most common elements
